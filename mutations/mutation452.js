1ba2387935add47189f949d78900af35d32acf1f
Phillip Campbell <spyyderz@gmail.com>
2010-10-10 00:23:46 -0400

    newest weights

---
+++ mutations/mutation452.js
@@ -0,0 +1,215 @@
+exports.weights = {
+  "attackConsideration": {
+    "hidden_weights": [
+      -0.181224056033343,
+      2.05854661868468,
+      0.70011433536543,
+      -0.191735237451557,
+      0.0592934801120035,
+      -0.63271743219415,
+      -0.0759923610585835,
+      0.0221667635605289,
+      0.498608704317166,
+      -1.94613532152665,
+      -0.612544138787948,
+      -1.36576124742866
+    ],
+    "input_weights": [
+      {
+        "isFriendly": -0.44391133584459,
+        "effDef": -1.67158175339277,
+        "isSelf": -0.0,
+        "incomingFriendlyFleets": 3.0564937490216,
+        "isNeutral": 0.0741756962604025,
+        "distanceThreeEnemyPlanets": 0.2,
+        "growth": -1.24439117443966,
+        "incomingEnemyFleets": -0.013468451858524,
+        "distanceThreeMyPlanets": -0.1,
+        "distance": 0.130622236080995,
+        "canTakeRightNow": -0.00316784993099199,
+        "isEnemy": 0.0035301958738345
+      },
+      {
+        "isFriendly": -0.117294202414776,
+        "effDef": -0.038621824343096,
+        "isSelf": -0.1,
+        "incomingFriendlyFleets": 0.118622706996022,
+        "isNeutral": -1.14343608219111,
+        "distanceThreeEnemyPlanets": 0.0,
+        "growth": -0.644150433308368,
+        "incomingEnemyFleets": -0.90971866151672,
+        "distanceThreeMyPlanets": 0.0,
+        "distance": 0.009945048883028,
+        "canTakeRightNow": -0.157999707638049,
+        "isEnemy": 3.2639293299871
+      },
+      {
+        "isFriendly": -0.322352162171102,
+        "effDef": -0.071672159120564,
+        "isSelf": 0.05,
+        "incomingFriendlyFleets": -0.32570966523096,
+        "isNeutral": 0.424774096222024,
+        "distanceThreeEnemyPlanets": 0.1,
+        "growth": 0.353608838000868,
+        "incomingEnemyFleets": -0.12146706921515,
+        "distanceThreeMyPlanets": 0.0,
+        "distance": 0.66169862579864,
+        "canTakeRightNow": 0.677118315993368,
+        "isEnemy": -0.315823656156465
+      },
+      {
+        "isFriendly": -0.145012852386974,
+        "effDef": 0.162551598161072,
+        "isSelf": -0.1,
+        "incomingFriendlyFleets": 0.067721934371937,
+        "isNeutral": -0.000396996025743945,
+        "distanceThreeEnemyPlanets": 0.0,
+        "growth": -0.055982515537635,
+        "incomingEnemyFleets": 1.08715803281,
+        "distanceThreeMyPlanets": 0.0,
+        "distance": 0.509474384127136,
+        "canTakeRightNow": 0.28298896130801,
+        "isEnemy": -0.00227786111733084
+      },
+      {
+        "isFriendly": -0.55758222183348,
+        "effDef": -0.108658772323579,
+        "isSelf": 0.05,
+        "incomingFriendlyFleets": 0.095697437950333,
+        "isNeutral": -2.0005261426306,
+        "distanceThreeEnemyPlanets": 0.025,
+        "growth": 0.430134379583228,
+        "incomingEnemyFleets": -0.430522319607654,
+        "distanceThreeMyPlanets": -0.05,
+        "distance": 0.064600658299547,
+        "canTakeRightNow": 0.958724877211964,
+        "isEnemy": 0.036084235885834
+      },
+      {
+        "isFriendly": 0.214521867618617,
+        "effDef": 0.111362836089169,
+        "isSelf": 0.1,
+        "incomingFriendlyFleets": -0.515175249973118,
+        "isNeutral": -13.4673716921222,
+        "distanceThreeEnemyPlanets": -0.1,
+        "growth": -0.0643956023863254,
+        "incomingEnemyFleets": -0.408626142909326,
+        "distanceThreeMyPlanets": 0.0,
+        "distance": 21.9383885062126,
+        "canTakeRightNow": 0.259329860700585,
+        "isEnemy": -0.0346858211601343
+      },
+      {
+        "isFriendly": 0.086788599757663,
+        "effDef": 1.56633243667812,
+        "isSelf": -0.1,
+        "incomingFriendlyFleets": -1.34884114046041,
+        "isNeutral": 5.8689227625746,
+        "distanceThreeEnemyPlanets": 0.2,
+        "growth": 0.166678692188879,
+        "incomingEnemyFleets": 0.144452120011585,
+        "distanceThreeMyPlanets": -0.05,
+        "distance": 0.00509352412822871,
+        "canTakeRightNow": -0.0212092169783516,
+        "isEnemy": -0.370881812670496
+      },
+      {
+        "isFriendly": 0.159937639330463,
+        "effDef": 0.9319370394555,
+        "isSelf": -0.4,
+        "incomingFriendlyFleets": 0.432708425973594,
+        "isNeutral": -1.17739422765665,
+        "distanceThreeEnemyPlanets": -0.2,
+        "growth": -0.043459987308219,
+        "incomingEnemyFleets": 0.331325246450326,
+        "distanceThreeMyPlanets": 0.0,
+        "distance": 0.789770374360056,
+        "canTakeRightNow": 0.0269987982445953,
+        "isEnemy": 0.93287586703911
+      },
+      {
+        "isFriendly": -0.391623825249622,
+        "effDef": -0.194324632291453,
+        "isSelf": 0.0,
+        "incomingFriendlyFleets": 0.189796666336514,
+        "isNeutral": 2.12471656682708,
+        "distanceThreeEnemyPlanets": -0.0,
+        "growth": -0.041805077047832,
+        "incomingEnemyFleets": -0.244256902467216,
+        "distanceThreeMyPlanets": 0.2,
+        "distance": -0.07703444026551,
+        "canTakeRightNow": 2.26341168546378,
+        "isEnemy": 0.0963741143111414
+      },
+      {
+        "isFriendly": 0.119555745190409,
+        "effDef": 0.64596700172209,
+        "isSelf": 0.1,
+        "incomingFriendlyFleets": 2.59909755555956,
+        "isNeutral": 0.080147622749038,
+        "distanceThreeEnemyPlanets": 0.0,
+        "growth": -0.192902025380652,
+        "incomingEnemyFleets": 0.154308103886322,
+        "distanceThreeMyPlanets": -0.2,
+        "distance": 0.185683305212544,
+        "canTakeRightNow": 0.303475610277906,
+        "isEnemy": -0.234788465791952
+      },
+      {
+        "isFriendly": -0.149911122995272,
+        "effDef": 0.066610053741002,
+        "isSelf": -0.1,
+        "incomingFriendlyFleets": -0.15851993619969,
+        "isNeutral": 0.144436973403475,
+        "distanceThreeEnemyPlanets": -0.8,
+        "growth": -0.0268163774729908,
+        "incomingEnemyFleets": -0.107689416558484,
+        "distanceThreeMyPlanets": 0.0,
+        "distance": -0.070775883040555,
+        "canTakeRightNow": 0.710110584606124,
+        "isEnemy": -0.0925947251551376
+      },
+      {
+        "isFriendly": -0.133073773650503,
+        "effDef": 0.07660516918001,
+        "isSelf": -0.1,
+        "incomingFriendlyFleets": -0.368519205238324,
+        "isNeutral": 0.823208982282676,
+        "distanceThreeEnemyPlanets": 0.0,
+        "growth": 0.272254775787954,
+        "incomingEnemyFleets": -0.0826993307775472,
+        "distanceThreeMyPlanets": 0.0,
+        "distance": 0.20657914615484,
+        "canTakeRightNow": 0.0118054527734589,
+        "isEnemy": -0.50232885010849
+      }
+    ]
+  },
+  "decisionConsideration": {
+    "hidden_weights": [
+      -0.176154677056484,
+      0.219647490369832,
+      3.49183237027684,
+      1.90636132576711
+    ],
+    "input_weights": [
+      {
+        "ships": -1.03273503654184,
+        "growth": -0.512994247708096
+      },
+      {
+        "ships": 1.37582281908067,
+        "growth": 15.317980357122
+      },
+      {
+        "ships": -0.597127545660046,
+        "growth": -0.11733351078502
+      },
+      {
+        "ships": -0.079427349327657,
+        "growth": -0.327187154042823
+      }
+    ]
+  },
+  "created_on": "Sun Oct 10 00:13:36 -0400 2010"
+}
